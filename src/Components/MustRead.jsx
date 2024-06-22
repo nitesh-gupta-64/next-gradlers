330b613573aab1e462af97f012c8e4f90f893ef5
nitesh-gupta-64 <123109031@nitkkr.ac.in>
2024-06-23 00:23:40 +0530

    done

--- src/Components/MustRead.jsx
+++ src/Components/MustRead.jsx
@@ -5,86 +5,94 @@ import { useRouter } from "next/navigation";
 const MustRead = ({ mb, show, show1, show2, setShow, setShow1, setShow2 }) => {
   const router = useRouter();
   return (
-    <div>
+    <>
       {mb && (
-        <>
-          <section>
-            <h2>MUST READ</h2>
-          </section>
-          {show1 || show2 ? (
-            <div>
-              {show1 && (
-                <div
-                  onClick={() => {
-                    router.push(
-                      `${show1.slug}                    `
-                    );
-                  }}
-                >
-                  <img src={show1.image} />
-                  <div>
-                    <p>{show1.seo && show1.seo.seoauthor}</p>
-                    <ul>
-                      <li>
-                        <p>
-                          {show1.createdAt &&
-                            new Date(show1.createdAt.toDate()).getDate()}{" "}
-                          {show1.createdAt &&
-                            new Date(show1.createdAt.toDate()).toLocaleString(
-                              "default",
-                              { month: "short" }
-                            )}
-                          ,{" "}
-                          {show1.createdAt &&
-                            new Date(show1.createdAt.toDate()).getFullYear()}
-                        </p>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              )}
-              {show2 && (
-                <div
-                  onClick={() => {
-                    router.push(
-                      `${show2.slug}
+        <div>
+          {mb && (
+            <>
+              <section>
+                <h2>MUST READ</h2>
+              </section>
+              {show1 || show2 ? (
+                <div>
+                  {show1 && (
+                    <div
+                      onClick={() => {
+                        router.push(`${show1.slug}                    `);
+                      }}
+                    >
+                      <img src={show1.image} />
+                      <div>
+                        <p>{show1.seo && show1.seo.seoauthor}</p>
+                        <ul>
+                          <li>
+                            <p>
+                              {show1.createdAt &&
+                                new Date(
+                                  show1.createdAt.toDate()
+                                ).getDate()}{" "}
+                              {show1.createdAt &&
+                                new Date(
+                                  show1.createdAt.toDate()
+                                ).toLocaleString("default", { month: "short" })}
+                              ,{" "}
+                              {show1.createdAt &&
+                                new Date(
+                                  show1.createdAt.toDate()
+                                ).getFullYear()}
+                            </p>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                  )}
+                  {show2 && (
+                    <div
+                      onClick={() => {
+                        router.push(
+                          `${show2.slug}
                       `
-                    );
-                  }}
-                >
-                  <img src={show2.image} />
-                  <div>
-                    <p>{show2.seo && show2.seo.seoauthor}</p>
-                    <ul>
-                      <li>
-                        <p>
-                          {show2.createdAt &&
-                            new Date(show2.createdAt.toDate()).getDate()}{" "}
-                          {show2.createdAt &&
-                            new Date(show2.createdAt.toDate()).toLocaleString(
-                              "default",
-                              { month: "short" }
-                            )}
-                          ,{" "}
-                          {show2.createdAt &&
-                            new Date(show2.createdAt.toDate()).getFullYear()}
-                        </p>
-                      </li>
-                    </ul>
-                  </div>
+                        );
+                      }}
+                    >
+                      <img src={show2.image} />
+                      <div>
+                        <p>{show2.seo && show2.seo.seoauthor}</p>
+                        <ul>
+                          <li>
+                            <p>
+                              {show2.createdAt &&
+                                new Date(
+                                  show2.createdAt.toDate()
+                                ).getDate()}{" "}
+                              {show2.createdAt &&
+                                new Date(
+                                  show2.createdAt.toDate()
+                                ).toLocaleString("default", { month: "short" })}
+                              ,{" "}
+                              {show2.createdAt &&
+                                new Date(
+                                  show2.createdAt.toDate()
+                                ).getFullYear()}
+                            </p>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                  )}
                 </div>
+              ) : (
+                <div>No more Blogs related to this Category</div>
               )}
-            </div>
-          ) : (
-            <div>No more Blogs related to this Category</div>
+            </>
           )}
-        </>
+          <p>Request a Call Back</p>
+          <section>
+            <Form />
+          </section>
+        </div>
       )}
-      <p>Request a Call Back</p>
-      <section>
-        <Form />
-      </section>
-    </div>
+    </>
   );
 };
 
--- src/app/gmat/page.jsx
+++ src/app/gmat/page.jsx
@@ -1,17 +1,20 @@
-import React from "react";
+"use client"
+import React, { useContext } from "react";
 import styles from "./Gmat.module.css";
 import GMAT from "@/Components/GMAT/GMAT";
 import TestimonialsSwiper from "@/Components/Testimonials/Testimonials";
 import GmatWhat from "@/Components/GmatWhat/GmatWhat";
+import { AppContext } from "@/Context/AppContext";
 
 const page = () => {
+  const { admitInfo } = useContext(AppContext);
   return (
     <div className={styles.gmat}>
       <GMAT />
-      <GmatWhat/>
+      <GmatWhat />
       <div className={styles.testi}>
         <p>What Our Achievers Say</p>
-        <TestimonialsSwiper />
+        <TestimonialsSwiper data={admitInfo} />
       </div>
     </div>
   );
